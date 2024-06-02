9e047b7e383a32f0f5b30615a78fbcd157b54a37
Michael Owen <154030843+michaelo48@users.noreply.github.com>
2024-06-01 21:45:39 -0400

    Time: 134 ms (45.77%) | Memory: 19.5 MB (8.18%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -1,12 +1,7 @@
 class Solution(object):
     def reverseString(self, s):
-        left = 0
-        right = len(s)-1
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left += 1
-            right -= 1  
-        return s
+        for i in range(len(s)//2):
+            s[i], s[~i] = s[~i], s[i]
         """
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
