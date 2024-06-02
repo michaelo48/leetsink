eb146fe2fb002304094c3c1815490f3a0e45229f
Michael Owen <154030843+michaelo48@users.noreply.github.com>
2024-06-01 21:46:27 -0400

    Time: 119 ms (96.78%) | Memory: 19.4 MB (21.47%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -1,7 +1,6 @@
 class Solution(object):
     def reverseString(self, s):
-        for i in range(len(s)//2):
-            s[i], s[~i] = s[~i], s[i]
+        return s.reverse()
         """
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
